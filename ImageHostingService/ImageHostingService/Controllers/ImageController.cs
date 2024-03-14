6fbed40805d98f6fe16df2b7452fef43899bd0be
Vladimir Iakovlev <wwevovancik@gmail.com>
2024-03-14 15:26:51 +0300

    ImageHost

--- ImageHostingService/ImageHostingService/Controllers/ImageController.cs
+++ ImageHostingService/ImageHostingService/Controllers/ImageController.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
 
 namespace ImageHostingService.Controllers
 {
@@ -7,9 +10,27 @@ namespace ImageHostingService.Controllers
     public class ImageController : ControllerBase
     {
         [HttpPost]
-        public IActionResult CreateImage(FormFile file)
+        public async Task<IActionResult> GetImage(IFormFile uploadedFile)
         {
-            return Ok();
+            if (uploadedFile != null)
+            {
+                // путь к папке Files
+                string path = "/Files/" + uploadedFile.FileName;
+                // сохраняем файл в папку Files в каталоге wwwroot
+                using (var fileStream = new FileStream(path, FileMode.Create))
+                {
+                    await uploadedFile.CopyToAsync(fileStream);
+                }
+                return Ok(path);
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("/{FileName}")]
+        public ActionResult Image(string FileName)
+        {
+            string path = Path.GetFullPath("/Files/" + FileName); //validate the path for security or use other means to generate the path.
+            return PhysicalFile(path, "image/jpeg");
         }
     }
 }
